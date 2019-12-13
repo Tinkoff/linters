7e63370080b028acf6a91f692b1cc4bdd90bfe42
Dmitry Naumov <d.naumov@tinkoff.ru>
2019-12-13 18:54:14 +0300

    docs: fix prettier config filename typo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: [
-        './eslint/base/prettier',
-        './eslint/angular',
+        './packages/linters/eslint/base/prettier',
+        './packages/linters/eslint/angular',
     ],
 }
