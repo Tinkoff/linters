634541fb042734b0a188d5d72cb10be4cbafa0a9
o.drapeza <o.drapeza@tinkoff.ru>
2020-06-08 13:30:03 +0300

    feat(eslint-plugin-tinkoff): split eslint-plugin-tinkoff

---
+++ packages/eslint-config/src/internal/base.ts
@@ -0,0 +1,74 @@
+module.exports = {
+  extends: ['eslint-config-airbnb-base', 'plugin:eslint-comments/recommended'],
+
+  parser: 'babel-eslint',
+
+  settings: {
+    'import/parser': 'babel-eslint',
+  },
+
+  plugins: ['babel'],
+
+  parserOptions: {
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+
+  rules: {
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+      },
+    ],
+    'no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: false,
+        variables: true,
+      },
+    ],
+    'func-name-matching': 'warn',
+    'global-require': 'warn',
+    'class-methods-use-this': 'warn',
+    'no-continue': 'off',
+    'no-restricted-syntax': 'warn',
+    'default-case': 'warn',
+    'no-plusplus': ['warn', { allowForLoopAfterthoughts: true }],
+    'consistent-return': 'warn',
+    'vars-on-top': 'warn',
+    'no-var': 'warn',
+    camelcase: [
+      'warn',
+      {
+        allow: ['^UNSAFE_'],
+        ignoreDestructuring: false,
+        properties: 'never',
+      },
+    ],
+    'func-style': [
+      'error',
+      'declaration',
+      {
+        allowArrowFunctions: true,
+      },
+    ],
+    'max-depth': ['warn', 4],
+    'max-params': ['warn', 3],
+    'max-classes-per-file': ['error', 4],
+    complexity: ['error', 25],
+    'max-statements': ['error', 25],
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'no-underscore-dangle': 'off',
+    'no-return-assign': ['error', 'except-parens'],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'spaced-comment': ['error', 'always', { exceptions: ['*'] }],
+    'max-nested-callbacks': ['error', 4],
+    'no-bitwise': 'warn',
+    'no-useless-escape': 'warn',
+    'no-await-in-loop': 'off',
+  },
+};
---
+++ packages/eslint-config/src/lib.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['./index'],
+
+  plugins: [],
+
+  rules: {
+    'import/no-dynamic-require': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+  },
+};
