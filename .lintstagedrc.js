6be242dd491d61c94dcc4cde141eae1510991793
splincode <omaxphp@yandex.ru>
2021-11-03 12:47:36 +0300

    docs: improved README.md

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,5 @@
 module.exports = {
     'packages/**/**/*.{ts,js}': ['eslint --fix'],
-    '**/README.md': [
-        files => files.map(file => `markdown-toc -i ${file}`), // single file per command
-        'prettier --write',
-        'git add' // fixes 'Changes not staged for commit'
-    ],
-    '**/*.{md,json,template.html}': ['prettier --write --loglevel=warn']
-}
+    '**/README.md': ['prettier --write', 'git add'],
+    '**/*.{md,json,template.html}': ['prettier --write --loglevel=warn'],
+};
