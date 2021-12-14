05bb880e8f1967294d9cda71b6a2eeca493e72a0
Maksim Ivanov <omaxphp@yandex.ru>
2021-12-14 18:24:44 +0300

    feat(eslint-config-angular): support html files (#162)

--- packages/eslint-config-angular/index.js
+++ packages/eslint-config-angular/index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     './internal/base',
     './internal/base-typescript',
+    './internal/html',
     './internal/import',
     './internal/member-ordering',
     './internal/line-statements',
