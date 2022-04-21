8b1b93a5825fdb1a40dd1388dddd5bbbc592362f
splincode <omaxphp@yandex.ru>
2022-04-21 12:25:51 +0300

    fix(eslint-config-angular): added recommended rules of best practices

--- packages/eslint-config-angular/rxjs/index.js
+++ packages/eslint-config-angular/rxjs/index.js
@@ -5,7 +5,17 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       plugins: ['rxjs'],
       extends: ['plugin:rxjs/recommended'],
-      rules: {},
+      rules: {
+        'rxjs/no-compat': 'error',
+        'rxjs/no-connectable': 'error',
+        'rxjs/no-cyclic-action': 'error',
+        'rxjs/no-ignored-observable': 'error',
+        'rxjs/no-topromise': 'error',
+        'rxjs/no-unsafe-catch': 'error',
+        'rxjs/no-unsafe-first': 'error',
+        'rxjs/no-unsafe-switchmap': 'error',
+        'rxjs/throw-error': 'error',
+      },
     },
   ],
 };
