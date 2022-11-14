839cfd26c50aa96cddd566468ca03150d85731ff
Ilya Vladimirovich Zyablitsev <i.zyablitsev@tinkoff.ru>
2022-11-14 12:40:34 +0300

    feat(eslint-config-angular): allow ignoring by void
    floating promises may be ignored by adding leading "void" keyword

--- packages/eslint-config-angular/promise/index.js
+++ packages/eslint-config-angular/promise/index.js
@@ -22,6 +22,8 @@ module.exports = {
           'error',
           { ignoreVoid: true, ignoreIIFE: true },
         ],
+        // for compat with @typescript-eslint/no-floating-promises ignoreVoid
+        'no-void': ['error', { allowAsStatement: true }],
       },
     },
   ],
