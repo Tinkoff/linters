3ba1129a47f8b256e2e79017b2102e6580c0b701
Alex Kvak <metalalisa@gmail.com>
2023-04-14 15:03:16 +0300

    refactor: remove deprecated `@tinkoff/linters` package (#241)

--- packages/linters/eslint/angular/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: ['../common/typescript/enviroment', './conventions'],
-};
--- packages/linters/eslint/base/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: [
-    '../common/index.js',
-    'prettier', // our rules must be overridden by "eslint-config-prettier" to be completely conflict-free
-    'prettier/@typescript-eslint',
-  ],
-};
